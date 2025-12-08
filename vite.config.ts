d1e3acdb28a9b03ff5d8da594fbf64be226c387c
Somsak Rakjoi <somsak.rak@ttbbank.com>
2025-12-08 15:07:46 +0700

    remove react-router plugin from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { reactRouter } from '@react-router/dev/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), reactRouter()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': '/src',
