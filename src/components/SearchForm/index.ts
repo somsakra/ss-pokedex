38d912fe666c62ce8fb8a5951123f3d6757cceaa
Somsak Rakjoi <somsak.rak@ttbbank.com>
2025-12-08 17:26:12 +0700

    add ui form

---
+++ src/components/SearchForm/index.ts
@@ -0,0 +1,2 @@
+export { default } from './SearchForm'
+export { userSearchForm } from './SearchForm.hook'
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react"
 import { pokemonListService, pokemonDetailService } from "@/services";
+import SearchForm from "@/components/SearchForm";
 
 const getData = async () => {
     const response = await pokemonDetailService.getPokemonDetail('ditto');
@@ -13,7 +14,12 @@ const HomePage = () => {
     }, [])
     
   return (
-    <div>HomePage</div>
+    <div className="w-[90%] m-auto max-w-[1100px]">
+        <div className="flex justify-center">
+            <img src="/images/logo.webp" className="max-h-[80px] mt-[20px]" alt="" />
+        </div>
+        <SearchForm/>
+    </div>
   )
 }
 export default HomePage
